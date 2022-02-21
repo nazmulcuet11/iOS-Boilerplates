4e35a35ac29148ac40740d9300e1295713bbdaf6
nazmulcuet11 <nazmulcuet11@gmail.com>
2022-02-21 21:10:05 +0600

    inital commit

---
+++ iOS-Boilerplates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Boilerplates
+//
+//  Created by Nazmul Islam on 21/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
